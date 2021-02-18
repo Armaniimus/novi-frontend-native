f689398305278974e32ac858661c976cb54f0971
hi <tommy_chester@hotmail.com>
2021-02-18 16:42:36 +0100

    removed redundant lines and now uses the longincontext and songtext screen

--- App.js
+++ App.js
@@ -1,31 +1,30 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
+
+import LoginContextProvider from './context/LoginContextProvider';
 
 import Home from './screens/Home';
-import Profile from './screens/Profile';
+import Songtekst from './screens/Songtekst';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      {/* <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View> */}
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <LoginContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ title: 'Home' }}
+          />
+          <Stack.Screen name="Songtekst pagina" component={Songtekst} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </LoginContextProvider>
   );
 }
 
